fc2540c5e5578a0e241fb7a9cef9dfe07cc5180c
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-04-03 14:14:42 +0200

    fix: Refactor PersonalForm.jsx
    Enhanced accessibility, ensured form handling

--- CVCraft/src/components/InfoForm/PersonalData/PersonalForm.jsx
+++ CVCraft/src/components/InfoForm/PersonalData/PersonalForm.jsx
@@ -1,47 +1,56 @@
-/**
- * PersonalDataStyle.css includes styles for the form components.
- */
 import "@styles/InfoForm/PersonalData/personal_data_style.css";
-/**
- * UUID for generating unique keys.
- */
-import { v4 as uuidv4 } from "uuid";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-/**
- * Represents a single input field within a form.
- *
- * @param {string} fieldId - The unique identifier for the input field, used for both 'id' and 'name' attributes.
- * @returns {JSX.Element} An input element with associated labels.
- */
-function InputField({ fieldId }) {
-    return <input type={"text"} id={fieldId} name={fieldId} />;
+
+function InputField({ fieldId, value, onChange }) {
+    return <input type={"text"}
+                  id={fieldId}
+                  name={fieldId}
+                  value={value}
+                  onChange={onChange} />;
 }
 
 InputField.propTypes = {
     fieldId: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
 };
 
-/**
- * A form component containing inputs for personal information such as full name, email, and phone number.
- * It maps through an array of field data to render InputField components with labels.
- *
- * @returns {JSX.Element} A div containing a list of labeled input fields.
- */
 function PersonalForm() {
+    const [formData, setFormData] = useState({
+        fullname: '',
+        email: '',
+        number: ''
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
     const personalInputLabels = [
-        { id: uuidv4(), data: "Full name: ", fieldName: "fullname" },
-        { id: uuidv4(), data: "Email: ", fieldName: "email" },
-        { id: uuidv4(), data: "Phone number: ", fieldName: "number" },
+        { label: "Full name: ", fieldName: "fullname" },
+        { label: "Email: ", fieldName: "email" },
+        { label: "Phone number: ", fieldName: "number" }
     ];
 
     return (
         <div className={"personal-info"}>
             <ul>
-                {personalInputLabels.map((label) => (
-                    <li key={label.id}>
-                        <label htmlFor={label.fieldName}>{label.data}</label>
-                        <InputField fieldId={label.fieldName} />
+                {personalInputLabels.map((label, index) => (
+                    <li key={index}>
+                        <label htmlFor={label.fieldName}>{label.label}</label>
+                        <input
+                            type="text"
+                            id={label.fieldName}
+                            name={label.fieldName}
+                            value={formData[label.fieldName]}
+                            onChange={handleInputChange}
+                        />
                     </li>
                 ))}
             </ul>
