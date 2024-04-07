08651095cf415c9450815465d2098db5c6ed302f
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-04-07 13:57:18 +0200

    feat: Implement work experience mapping
    Possibility of adding and managing work experience

--- CVCraft/src/components/InputDocs/Document.jsx
+++ CVCraft/src/components/InputDocs/Document.jsx
@@ -21,55 +21,61 @@ function Document({ personalData, skills, education, experience }) {
 
             <div className={"document"}>
 
-                <div className={"header"}>
-                    <h1>{personalData.fullname}</h1>
-                    <h3>{personalData.position}</h3>
-                </div>
-
-                <aside>
-                    <h2>Contact Information</h2>
-                    <address>
-                        <div className={"email-wrapper"}>
-                            <MailIcon size={24}/>
-                            <p><a href={"mailto:" + personalData.email}>{personalData.email}</a></p>
-                        </div>
-
-                        <div className={"phone-wrapper"}>
-                            <PhoneIcon size={24}/>
-                            <p><a href={"tel:" + personalData.number}>{personalData.number}</a></p>
-                        </div>
-
-                        <div className={"skills-wrapper"}>
-                            <h2>Skills</h2>
-                            <ul>
-                                {skills.map((skill, index) => (
-                                    <li key={index}>{skill}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    </address>
-
-                    <h2>Education</h2>
-                    <div className={"edu-wrapper"}>
-                        {education.map((entry, index) => (
-                            <div key={index} className={"institution"}>
-                                <h3>{entry.eduInstitution}</h3>
-                                <p>{entry.major}</p>
-                                <span>{entry.startDate.slice(0, 4)} | {entry.endDate.slice(0, 4)}</span>
-                            </div>
-                        ))}
+                <div className={"side-section"}>
+                    <div className={"header"}>
+                        <h1>{personalData.fullname}</h1>
+                        <h3>{personalData.position}</h3>
                     </div>
-                </aside>
 
-                <div className={"prior-work"}>
-                    {experience.map((exp, index) => (
-                        <div key={index}>
-                            <h3>{exp.company}</h3>
-                            <p>{exp.position}</p>
-                            <span>{exp.description}</span>
+                    <div className={"inner-separation"}>
+                        <aside>
+                            <h2>Contact Information</h2>
+                            <address>
+                                <div className={"email-wrapper"}>
+                                    <MailIcon size={24}/>
+                                    <p><a href={"mailto:" + personalData.email}>{personalData.email}</a></p>
+                                </div>
+
+                                <div className={"phone-wrapper"}>
+                                    <PhoneIcon size={24}/>
+                                    <p><a href={"tel:" + personalData.number}>{personalData.number}</a></p>
+                                </div>
+
+                                <div className={"skills-wrapper"}>
+                                    <h2>Skills</h2>
+                                    <ul>
+                                        {skills.map((skill, index) => (
+                                            <li key={index}>{skill}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </address>
+
+                            <h2>Education</h2>
+                            <div className={"edu-wrapper"}>
+                                {education.map((entry, index) => (
+                                    <div key={index} className={"institution"}>
+                                        <h3>{entry.eduInstitution}</h3>
+                                        <p>{entry.major}</p>
+                                        <span>{entry.startDate.slice(0, 4)} | {entry.endDate.slice(0, 4)}</span>
+                                    </div>
+                                ))}
+                            </div>
+                        </aside>
+
+                        <div className={"prior-work"}>
+                            <h2>Prior experience</h2>
+                            {experience.map((exp, index) => (
+                                <div key={index}>
+                                    <h3>{exp.company}</h3>
+                                    <p>{exp.position}</p>
+                                    <span>{exp.description}</span>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
                 </div>
+
             </div>
         </>
     )
