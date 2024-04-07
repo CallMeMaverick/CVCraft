1eac5753ff05cf3849be022fcc2ed784d3931d58
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-04-07 15:38:17 +0200

    style: Make responsive

--- CVCraft/src/main.jsx
+++ CVCraft/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Form from "./components/InfoForm/Form.jsx";
+import './styles/index.css'
+import "@styles/InfoForm/input_fields.css"
+import "@styles/InputDocs/document.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Form />
+      {/*<div className={"document"}>*/}
+
+      {/*</div>*/}
   </React.StrictMode>,
 )
